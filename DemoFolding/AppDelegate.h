d134c641ed7a58f1a31fce30ac473ee4d7b76670
Vladislav Sinitsyn <shinisaru@shinisaru-macbook.local>
2013-07-30 13:35:06 +0200

    Initial Commit

---
+++ DemoFolding/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DemoFolding
+//
+//  Created by Vladislav Sinitsyn on 30.07.13.
+//  Copyright (c) 2013 Vladislav Sinitsyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
